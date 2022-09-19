ab96709ba0855d4826eb58373875c36bdbb85914
Dương Kafka Đinh Hoàng <47100782+KafkaWannaFly@users.noreply.github.com>
2022-09-19 16:50:48 +0700

    ErrorMessage

---
+++ WibuTubeConverter/Controls/ErrorMessage.cs
@@ -0,0 +1,40 @@
+﻿using static CommunityToolkit.Maui.Markup.GridRowsColumns;
+
+namespace WibuTubeConverter.Controls
+{
+    [AllBindableProps]
+    public partial class ErrorMessage : ContentView
+    {
+        string text;
+
+        public ErrorMessage()
+        {
+            Content = new Border
+            {
+                BindingContext = this,
+                Stroke = Colors.Red,
+                BackgroundColor = Color.FromRgba("#bb00001A"),
+                Padding = new Thickness(8),
+                Content = new Grid
+                {
+                    ColumnDefinitions = Columns.Define(Auto, Star),
+                    ColumnSpacing = 8,
+                    Children =
+                    {
+                        new Image()
+                        {
+                            Source = ImageSource.FromFile("warning.png"),
+                            WidthRequest = 20,
+                        }.Column(0),
+                        new Label
+                        {
+                            TextColor = Colors.Red
+                        }
+                            .Column(1)
+                            .Bind(Label.TextProperty, nameof(Text)),
+                    }
+                }
+            };
+        }
+    }
+}
--- WibuTubeConverter/Controls/TextInput.cs
+++ WibuTubeConverter/Controls/TextInput.cs
@@ -1,11 +1,4 @@
-﻿using CommunityToolkit.Maui.Markup;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WibuTubeConverter.Controls
+﻿namespace WibuTubeConverter.Controls
 {
     [AllBindableProps]
     public partial class TextInput : ContentView
